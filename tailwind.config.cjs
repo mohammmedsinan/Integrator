e9637b9af4db20bb01156d6acac1e671127ce340
mohammmedsinan <mohammedbusiness1099@gmail.com>
2023-07-07 14:21:09 +0300

    Integrator Setup with astro

---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--aw-color-primary)',
+        secondary: 'var(--aw-color-secondary)',
+        accent: 'var(--aw-color-accent)',
+        primaryI: 'var(--int-color-primary)',
+        secondaryI: 'var(--int-color-secondary)',
+        accentI: 'var(--int-color-accent)',
+      },
+      fontFamily: {
+        sans: ['var(--aw-font-sans)', ...defaultTheme.fontFamily.sans],
+        serif: ['var(--aw-font-serif)', ...defaultTheme.fontFamily.serif],
+        heading: ['var(--aw-font-heading)', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+  darkMode: 'class',
+};
