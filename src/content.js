780c4ac85e5cfa9d80afa5592f8ba68e6dacfce6
mohammmedsinan <mohammedbusiness1099@gmail.com>
2023-07-23 02:23:36 +0300

    Split astro component to widget

---
+++ src/content.js
@@ -0,0 +1,15 @@
+export const content = {
+    landingPage: {
+        hero: {
+            title: "is free open-source software & ease to ues",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, eveniet cumque iste repellat optio dolorum eum illo earum aliquid cum delectus itaque aliquam. Nobis, rem molestias ab id recusandae autem ullam consectetur reiciendis eveniet consequatur exercitationem soluta quis fuga. Magnam blanditiis omnis odio consectetur totam obcaecati saepe, incidunt repudiandae corrupti ab ratione sed sit asperiores odit vero hic unde cum earum aspernatur a eligendi. Incidunt, fuga? Sequi facilis perferendis voluptatibus?",
+            heroLinks:{link1:"#1",link2:"#2"}
+        },
+        offer: {
+            title: "What does ",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nobis dicta laborum ad dolor enim quasi facere quia, recusandae velit doloribus consequuntur et tenetur obcaecati harum magnam commodi vitae! Quo!",
+            
+            
+        }
+    }
+}
